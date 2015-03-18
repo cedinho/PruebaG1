5d1dd0d430bee5f21a7ffe3d2a24e066837b82d7
alumno04 <alumno04@TPP-LAB04-17.TPP.UPEU.EDU.PE>
2015-03-18 10:33:08 -0500

    se creo la clase ciro y se subio por primera vez

---
+++ PruebaLp2G1/src/pruebalp2g1/cedinho.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebalp2g1;
+
+/**
+ *
+ * @author alumno04
+ */
+public class cedinho {
+    
+    String nombre;
+    int edad;
+    int dni;
+    
+}
